e0ae773572e84fc5310f9b8114f5e0fc41f01a4f
elec139621 <85816256+elec139621@users.noreply.github.com>
2021-06-14 16:19:07 +0300

    Update Main.java

--- swing_jfrmae_label/Main.java
+++ swing_jfrmae_label/Main.java
@@ -7,6 +7,26 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// class Person
+		// private String m_fname
+		// private String m_lname
+		// private int m_age
+		// ctor with (m_fname, m_lname, m_age)
+		// getter + setter for all 3
+		// create toString
+		
+		// in main:
+		// scanner.next() --> fname
+		// scanner.next() --> lname
+		// scanner.nextInt() --> age
+		// create new Person with this values (hint: use ctor)
+		// create a JFrame
+		// create a label
+		// put the toString of the person into the label
+		// hint: String toString1 = person.toString();
+		// label = new JLabel(toString1)
+		
+		
 		JFrame frame = new JFrame(); 
 		frame.setSize(700, 700);
 		frame.setTitle("my first demo");
